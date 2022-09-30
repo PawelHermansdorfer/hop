26187954657b96d88f41bb313e731909598eb54d
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2022-09-30 18:26:46 +0200

    Update main

--- src/main.c
+++ src/main.c
@@ -1,51 +1,51 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-void scc(int error_code) // SDL check code
+void scc(int error_code)
 {
-  if(error_code < 0)
-  {
-    printf("SDL ERROR: %s\n", SDL_GetError());
-    exit(1);
-  }
+ if (error_code < 0)
+ {
+  printf("SDL ERROR: %s\n", SDL_GetError());
+  exit(1);
+ }
 }
 
-void *scp(void *ptr) // SDL check pointer
+void *scp(void *ptr)
 {
-  if (ptr == NULL)
-  {
-    printf("SDL ERROR: %s\n", SDL_GetError());
-  }
-  return ptr;
+ if(ptr == NULL)
+ {
+  printf("SDL ERROR: %s\n", SDL_GetError());
+ }
+ return ptr;
 }
-
 
 int main(int argc, char *argv[])
 {
-  scc(SDL_Init(SDL_INIT_VIDEO));
+ scc(SDL_Init(SDL_INIT_VIDEO));
 
-  SDL_Window *window = scp(SDL_CreateWindow("Window", 100, 100, 800, 600, SDL_WINDOW_RESIZABLE));
-  SDL_Renderer *renderer = scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+ SDL_Window *window = scp(SDL_CreateWindow("Window", 100, 100, 800, 600, SDL_WINDOW_RESIZABLE));
+ SDL_Renderer *renderer = scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
 
-  char quit = 0;
-  while(!quit)
+ char quit = 0;
+ while (!quit)
+ {
+  SDL_Event event = {0};
+  while (SDL_PollEvent(&event))
   {
-    SDL_Event event = {0};
-    while(SDL_PollEvent(&event))
+   switch(event.type)
+   {
+    case SDL_QUIT:
     {
-      switch(event.type)
-      {
-        case SDL_QUIT: {
-                         quit = 1;
-                       } break;
-      }
-    }
-
-    scc(SDL_SetRenderDrawColor(renderer, 3, 3, 3, 0));
-    scc(SDL_RenderClear(renderer));
-    SDL_RenderPresent(renderer);
+     quit = 1;
+    } break;
+   }
   }
 
-  SDL_Quit();
-  return 0;
+  scc(SDL_SetRenderDrawColor(renderer, 3, 3, 3, 0));
+  scc(SDL_RenderClear(renderer));
+  SDL_RenderPresent(renderer);
+ }
+
+ SDL_Quit();
+ return 0;
 }
