80dd1a5d362b684d4cc40a34e04c04180d96a3c8
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2022-10-17 21:02:35 +0200

    Update msvc build file

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,17 +1,30 @@
 @echo off
 
+
+REM Check os version
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT (
+    echo Working on 32bit operating system
+    set SDL2_LIB_DIR="\dependencies\SDL2\lib\x86"
+)
+if %OS%==64BIT (
+    echo Working on 64bit operating system
+    set SDL2_LIB_DIR="\dependencies\SDL2\lib\x64"
+)
+
+
 SET CFLAGS=/W4 /WX /std:c11 /wd4996 /wd5105 /FC /TC /Zi /nologo
 
 SET INCLUDES=/I ..\dependencies\SDL2\include ^
              /I ..\src\
 
-SET LIBS=..\dependencies\SDL2\lib\x64\SDL2.lib ^
-         ..\dependencies\SDL2\lib\x64\SDL2main.lib ^
+SET LIBS=..\%SDL2_LIB_DIR%\SDL2.lib ^
+         ..\%SDL2_LIB_DIR%\SDL2main.lib ^
          opengl32.lib User32.lib Gdi32.lib shell32.lib
 
 IF NOT EXIST .\build\ MKDIR .\build\
 IF NOT EXIST .\build\SDL2.dll (
-    COPY .\dependencies\SDL2\lib\x64\SDL2.dll .\build\
+    COPY .\%SDL2_LIB_DIR%\SDL2.dll .\build\
 )
 
 PUSHD .\build
