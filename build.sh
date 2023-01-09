a02be97be32ffd8a5851fdf490058a37703ac72b
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2023-01-09 19:13:41 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 PROJECT_NAME=main
 PROJECT_FILES="../src/*.c"
 PROJECT_INCLUDES="../src/"
-FLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
-LIBS='sdl2 sdl2_ttf glew'
+FLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb -lSDL2 -lSDL2_ttf"
+LIBS='sdl2 sdl2_ttf'
 
 if [ ! -d ./build ]; then
     mkdir ./build
@@ -13,7 +13,11 @@ if [ `uname` = "Darwin" ]; then
     FLAGS+=" -framework OpenGL"
 fi
 
+# copy fonts dir
+if [ ! -d ./build/fonts ]; then
+    cp -r ./fonts ./build/
+fi
 
 pushd ./build
-clang $FLAGS $PROJECT_FILES -o$PROJECT_NAME -I$PROJECT_INCLUDES `pkg-config --libs $LIBS`
+clang $FLAGS $PROJECT_FILES -o$PROJECT_NAME -I$PROJECT_INCLUDES `pkg-config --cflags --libs $LIBS`
 popd
